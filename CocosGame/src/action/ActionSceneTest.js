a85e2afc3d3e52d60abe2afe5f011c5cfc62a8c2
shenliang34 <971935832@qq.com>
2015-01-29 19:53:34 +0800

    贝塞尔曲线的动作

--- CocosGame/src/action/ActionSceneTest.js
+++ CocosGame/src/action/ActionSceneTest.js
@@ -18,7 +18,28 @@ var ActionScene = cc.Scene.extend({
 //		var action = cc.sequence(cc.jumpTo(1, cc.p(400, 300), 50, 4),cc.CallFunc(function(){cc.log("============end==========")},this,false));//添加动画完成停止返回函数
 //		g1.runAction(action)//跳跃时间 位置 高度 次数
 //		g1.runAction(cc.skewTo(1, 50, 10))//时间 x y
-		g1.runAction(cc.bezierTo(1, [cc.p(300, 300),cc.p(400, 300),cc.p(400, 400),cc.p(300, 400),cc.p(300, 300)]))
+//		g1.runAction(cc.bezierTo(1, [cc.p(300, 300),cc.p(400, 300)]));
+		var delay = cc.delayTime(0.25);
+
+		// 3 and only 3 control points should be used for Bezier actions.
+		var controlPoints = [ cc.p(0, 374),
+		                      cc.p(300, -374),
+		                      cc.p(300, 100) ];
+
+		var bezierForward = cc.bezierBy(2, controlPoints);
+		var rep = cc.sequence(bezierForward, delay, bezierForward.reverse(), delay.clone()).repeatForever();
+		var controlPoints2 = [ cc.p(200, 200),cc.p(400, 200),cc.p(400, 400),cc.p(200,400),cc.p(200,200)];
+		var bezierTo1 = cc.bezierTo(2, controlPoints2);
+//
+//		// // sprite 3
+//		var controlPoints3 = controlPoints2.slice();
+//		var bezierTo2 = cc.bezierTo(2, controlPoints3);
+
+//		g1.runAction(rep);
+		g1.x = 200;
+		g1.y = 200;
+		g1.runAction(bezierTo1)
+//		g1.runAction(cc.sequence(bezierTo1,be));
 		cc.log("-------------------------test---------------------")
 	}
 	
